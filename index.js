5501ffa951f34921b7185272f7c2304e58d159dd
Marius Patrascu <marius-florin.patrascu@ing.com>
2022-05-10 10:08:05 +0300

    add new mapping between the types that can be retrieved from backend and the tabs of the ui that will be displayed
    Signed-off-by: Marius Patrascu <marius-florin.patrascu@ing.com>

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ import {
   linksTypeIconMap
 } from './icons-and-data-mappings';
 
+import {
+  lineageViewsTypesMapping
+} from './navigation-types-mappings'
+
 export {
   iconsMap,
   itemGroupIconMap,
   getIconByGroup,
-  linksTypeIconMap
+  linksTypeIconMap,
+  lineageViewsTypesMapping
 };
---
+++ navigation-types-mappings.js
@@ -0,0 +1,10 @@
+/*
+* The navigation tables are displayed based on the map defined below.
+* If a type is missing from the map, it will have all the tabs displayed.
+*
+* */
+export const lineageViewsTypesMapping = {
+    "GlossaryTerm" : ['vertical-lineage'],
+    "Glossary" : ['vertical-lineage'],
+    'GlossaryCategory' : ['vertical-lineage']
+}
