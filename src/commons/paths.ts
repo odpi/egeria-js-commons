c91259266cf1661ec44e2dd0ca568bd4f6a489cb
Iliescu Cristian-Mihai <cristianmihaiiliescu@gmail.com>
2022-11-22 17:30:06 +0200

    fixed the export for asset lineage path function
    Signed-off-by: Iliescu Cristian-Mihai <cristianmihaiiliescu@gmail.com>

--- src/commons/paths.ts
+++ src/commons/paths.ts
@@ -5,14 +5,16 @@ const ASSET_CATALOG_PATH = '/assets/catalog';
 const LOGIN_PATH = '/login';
 const ABOUT_PATH = '/about';
 
-export {
-  ABOUT_PATH,
-  ASSET_CATALOG_PATH,
-  LOGIN_PATH
-}
-
-export function getAssetLineagePrintUri(guid: any, lineageType: any) {
+function getAssetLineagePrintUri(guid: any, lineageType: any) {
   const printUri = `/asset-lineage/${ guid }/${ lineageType }/print`;
 
   return printUri;
 }
+
+export {
+  ABOUT_PATH,
+  ASSET_CATALOG_PATH,
+  LOGIN_PATH,
+  getAssetLineagePrintUri
+}
+
