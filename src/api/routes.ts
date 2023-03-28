ed220294132ef43d3b17db639be272914038445a
Bogdan Sava <bogdan.sava@gmail.com>
2023-03-28 19:10:51 +0300

    using bearer token authorization
    Signed-off-by: Bogdan Sava <bogdan.sava@gmail.com>

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -3,7 +3,7 @@
  */
 const API_ASSETS_SEARCH_PATH = '/api/assets/search';
 const API_ASSETS_TYPES_PATH = '/api/assets/types';
-const API_LOGIN_PATH = '/api/auth/login';
+const API_LOGIN_PATH = '/api/token';
 
 export {
   API_ASSETS_SEARCH_PATH,
