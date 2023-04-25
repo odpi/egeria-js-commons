4839b3fb52a622fc438668e6d53644ac18715dc2
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2023-04-25 10:49:41 +0300

    Add ESLint
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- src/get-formatted-date.ts
+++ src/get-formatted-date.ts
@@ -1,11 +1,10 @@
 const getFormattedDate = (date: Date) => {
-
-  let month = date.getMonth() + 1;
-  let day = date.getDate();
-  let year = date.getFullYear();
-  let hour = date.getHours();
-  let min = date.getMinutes();
-  let sec = date.getSeconds();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
 
   return {
     year : year,
--- src/token/index.ts
+++ src/token/index.ts
@@ -10,11 +10,11 @@ export interface JWTInterface {
 export const parseJwt = (token: string | null): JWTInterface | null => {
   if(token !== null && token !== "") {
     try {
-      var base64Url = token.split('.')[1];
+      const base64Url = token.split('.')[1];
 
-      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
 
-      var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+      const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
       }).join(''));
 
